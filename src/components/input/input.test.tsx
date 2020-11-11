6b6d2f1d73036204d87c459e35bf2cc53ea0917b
luowendi <luowendi1996@gmail.com>
2020-11-11 12:05:39 +0800

    feat:input test

--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Input, { InputProps } from './Input';
 
 const defaultProps: InputProps = {
@@ -8,13 +8,32 @@ const defaultProps: InputProps = {
 };
 describe('test Input component', () => {
   it('should render the correct default Input', () => {
-    const wrapper = render(<Input {...defaultProps} />);
-    const testNode = wrapper.getByPlaceholderText(
+    render(<Input {...defaultProps} />);
+    const testNode = screen.getByPlaceholderText(
       'test-input'
     ) as HTMLInputElement;
     expect(testNode).toBeInTheDocument();
+    expect(testNode).toHaveClass('input-inner')
+    fireEvent.change(testNode, { target: { value: '123' } })
+    expect(defaultProps.onChange).toHaveBeenCalled()
+    expect(testNode.value).toEqual('123')
   });
-  it('should render the disabled Input on disabled property', () => {});
-  it('should render different input sizes on size property', () => {});
-  it('should render prepand and append element on prepand/append property', () => {});
+  it('should render the disabled Input on disabled property', () => {
+    render(<Input disabled placeholder="disabled" />)
+    const disbaledNode = screen.getByPlaceholderText('disabled') as HTMLInputElement
+    expect(disbaledNode).toBeInTheDocument()
+    expect(disbaledNode.disabled).toBeTruthy()
+  });
+  it('should render different input sizes on size property', () => {
+    const wrapper = render(<Input placeholder="sizes" size="large" />)
+    const container= wrapper.container.querySelector('.input-wrapper')
+    expect(container).toHaveClass('input-size-large')
+  });
+  it('should render prepand and append element on prepand/append property', () => {
+    const { queryByText, container } = render(<Input placeholder="pend" prepend="https://" append=".com" />)
+    const penNode = container.querySelector('.input-wrapper')
+    expect(penNode).toHaveClass('input-group input-group-append input-group-prepend')
+    expect(queryByText('https://')).toBeInTheDocument()
+    expect(queryByText('.com')).toBeInTheDocument()
+  });
 });
