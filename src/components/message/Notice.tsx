d95e1e76e70cbf10c38e98e0af309437fb01a436
luowendi <luowendi1996@gmail.com>
2020-11-16 18:43:52 +0800

    update:message component

--- src/components/message/Notice.tsx
+++ src/components/message/Notice.tsx
@@ -1,9 +1,74 @@
 import React from 'react';
+import classNames from 'classnames'
 
-interface NoticeProps {}
+export interface NoticeProps {
+  content: React.ReactNode
+  duration?: number
+  prefixCls: string
+  className?: string
+  onClose?: () => void
+}
 
-const Notice: React.FC<NoticeProps> = () => {
-  return null;
-};
+interface NoticeState {
+  shouldClose: boolean
+}
+
+class Notice extends React.Component<NoticeProps, NoticeState> {
+  private timer?: number
+  private closeTimer?: number | null
+
+  static defaultProps = {
+    duration: 3000
+  }
+
+  constructor (props:NoticeProps) {
+    super(props)
+    this.state = {
+      shouldClose: false
+    }
+  }
+  
+  public componentDidMount () {
+   this.startCloseTimer()
+  }
+
+  private close () {
+    this.clearCloseTimer()
+
+    this.timer = window.setTimeout(() => {
+      if (this.props.onClose) {
+        this.props.onClose()
+      }
+      clearTimeout(this.timer)
+    } ,300)
+  }
+
+  private startCloseTimer () {
+    if (this.props.duration) {
+      this.closeTimer = window.setTimeout(() => {
+         this.close()
+       }, this.props.duration - 300)
+    }
+  }
+
+  private clearCloseTimer () {
+    if (this.closeTimer) {
+      clearTimeout(this.closeTimer)
+      this.closeTimer = null
+    }
+  }
+
+  public render () {
+    const { prefixCls, className } = this.props
+    const classes = classNames(`${prefixCls}-notice`, className)
+    return <div className={classes}>
+      {this.props.content}
+    </div>
+  }
+
+  public componentWillUnmount () {
+    this.clearCloseTimer()
+  }
+}
 
 export default Notice;
--- src/components/message/Notification.tsx
+++ src/components/message/Notification.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Notice, { NoticeProps } from './Notice'
 
-interface NotificationProps {}
+interface NotificationProps {
+  prefixCls?: string;
+  className?: string;
+}
 
-interface NotificationState {}
+interface NotificationState {
+  notices: { notice: NoticeContent }[]
+}
+
+interface NoticeContent
+  extends Omit<NoticeProps, 'prefixCls' | 'onClose'> {
+  prefixCls?: string
+  key?: React.Key
+  onClose?: () => void
+}
+
+let noticeNumber = 0
+
+const getUuid = () => {
+  return `notification${new Date().getTime()}-${noticeNumber++}`
+}
 
 class Notification extends React.Component<
   NotificationProps,
   NotificationState
 > {
-  static newInstance: () => void;
+  static newInstance: () => void
+
+  static defaultProps = {
+    prefixCls: 'rc-notification'
+  }
+
+
+  public add(originNotice: NoticeContent) {
+    const key = originNotice.key || getUuid()
+    const notice: NoticeContent = {
+      ...originNotice,
+      key
+    }
+    const { notices } = this.state
+    const idx = notices.map((v) => v.notice.key).indexOf(key)
+    const updateNotices = notices.concat()
+    if (idx !== -1) {
+      updateNotices.splice(idx, 1, { notice })
+    }
+    this.setState({notices: updateNotices})
+  }
+
+  public remove (removeKey: React.Key) {
+    this.setState(({ notices }) => ({
+      notices: notices.filter(({ notice: { key } }) => key !== removeKey)
+    }))
+  }
+
+  private getNoticeDom () {
+    const { notices } = this.state
+    const {prefixCls} = this.props
+    return notices.map(v => {
+      const noticeProps = {
+        prefixCls,
+        ...v.notice
+      } as NoticeProps
+
+      const closeCb = () => {
+        this.remove(v.notice.key!)
+        if (v.notice.onClose) {
+          v.notice.onClose()
+        }
+      }
+
+      return <Notice {...noticeProps} onClose={closeCb}/>
+    })
+  }
   public render() {
-    return null;
+    return <div>{this.getNoticeDom()}</div>
   }
 }
 
 Notification.newInstance = () => {
-  const div = document.createElement('div');
-  document.body.appendChild(div);
-  ReactDOM.render(<Notification ref={() => {}} />, div);
-};
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(<Notification ref={() => {}} />, div)
+}
 
-export default Notification;
+export default Notification
