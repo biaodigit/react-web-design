f370d3e9591018ff733e12391883d3d77b60c011
biaodigit <15622285015@163.com>
2020-11-26 22:03:19 +0800

    update:autocomplete component

--- src/components/autocomplete/AutoComplete.tsx
+++ src/components/autocomplete/AutoComplete.tsx
@@ -8,17 +8,43 @@ export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
 
 const AutoComplete: FC<AutoCompleteProps> = (props) => {
     const { fetchSuggestions, onSelect, value, ...rest } = props
-    const [inputValue,setInputValue] = useState(value)
+    const [inputValue, setInputValue] = useState(value)
+    const [suggestions, setSuggestions] = useState<string[]>([])
 
-    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
-      const value = e.target.value
-      setInputValue(value)
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value
+        setInputValue(value)
+        if (value) {
+            const result = fetchSuggestions(value)
+            setSuggestions(result)
+        } else {
+            setSuggestions([])
+        }
     }
+
+    const handleSelect = (item: string) => {
+        setInputValue(item)
+        setSuggestions([])
+        if (onSelect) {
+            onSelect(item)
+        }
+    }
+
+    const generateDropdown = () => (
+        <ul>
+            {suggestions.map((item, index) => (
+                <li key={index} onClick={() => handleSelect(item)}>{item}</li>
+            ))}
+        </ul>
+    )
+
+    console.log(suggestions)
     return (
         <div className="auto-complete">
-            <Input value={value} onChange={handleChange} {...rest}/>
+            <Input value={inputValue} onChange={handleChange} {...rest} />
+            {suggestions.length > 0 ? generateDropdown() : null}
         </div>
     )
 }
 
-export default AutoComplete
+export default AutoComplete;
