6be559610d4cf6a6064aa03f230600774e62ccd0
biaodigit <15622285015@163.com>
2021-07-11 15:00:59 +0800

    update:upload component feat progress

--- src/components/upload/Upload.tsx
+++ src/components/upload/Upload.tsx
@@ -1,19 +1,47 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import axios from 'axios'
+import UploadList from './UploadList'
 import Button from '../button'
+
+export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error'
 
 export interface UploadProps {
     action: string
+    defaultFileList?: UploadFile[]
     beforeUpload?: (file: File) => boolean | Promise<File>
     onChange?: (file: File) => void
     onProgress?: (percentage: number, file: File) => void
     onSuccess?: (data: any, file: File) => void
     onError?: (err: any, file: File) => void
+    onRemove?: (file: UploadFile) => void
 }
 
+export interface UploadFile {
+    uid: string
+    size: number
+    name: string
+    status?: UploadFileStatus
+    percent?: number
+    raw?: File
+    response?: any
+    error?: any
+}
 const Upload: React.FC<UploadProps> = (props) => {
-    const { action, beforeUpload, onChange, onProgress, onSuccess, onError } = props
+    const { action, defaultFileList, beforeUpload, onChange, onProgress, onSuccess, onError, onRemove } = props
     const fileInput = useRef<HTMLInputElement>(null)
+    const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList || [])
+
+    const updateFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
+        setFileList(prevList => {
+            return prevList.map(file => {
+                if (file.uid === updateFile.uid) {
+                    return { ...file, ...updateObj }
+                } else {
+                    return file
+                }
+            })
+        })
+    }
 
     const handleClick = () => {
         if (fileInput.current) {
@@ -50,6 +78,15 @@ const Upload: React.FC<UploadProps> = (props) => {
     }
 
     const post = (file: File) => {
+        let _file: UploadFile = {
+            uid: Date.now() + 'upload_file',
+            status: 'ready',
+            name: file.name,
+            size: file.size,
+            percent: 0,
+            raw: file
+        }
+        setFileList([_file, ...fileList])
         const formData = new FormData()
         formData.append(file.name, file)
         axios.post(action, formData, {
@@ -58,11 +95,16 @@ const Upload: React.FC<UploadProps> = (props) => {
             },
             onUploadProgress: (e) => {
                 let percentage = Math.round((e.loaded * 100) / e.total) || 0
-                if (percentage < 100 && onProgress) {
-                    onProgress(percentage, file)
+                if (percentage < 100) {
+                    updateFileList(_file, { percent: percentage, status: 'uploading' })
+                    if (onProgress) {
+                        onProgress(percentage, file)
+                    }
                 }
+
             }
         }).then(res => {
+            updateFileList(_file, { status: 'success', response: res.data })
             if (onSuccess) {
                 onSuccess(res.data, file)
             }
@@ -70,6 +112,7 @@ const Upload: React.FC<UploadProps> = (props) => {
                 onChange(file)
             }
         }).catch(err => {
+            updateFileList(_file, { status: 'error', error: err })
             if (onError) {
                 onError(err, file)
             }
@@ -78,6 +121,16 @@ const Upload: React.FC<UploadProps> = (props) => {
             }
         })
     }
+
+    const handleRemove = (file: UploadFile) => {
+        setFileList(prevList => {
+            return prevList.filter(item => item.uid !== file.uid)
+        })
+        if (onRemove) {
+            onRemove(file)
+        }
+    }
+
     return (
         <div className="web-upload">
             <Button type="primary" onClick={handleClick}>upload file</Button>
@@ -89,6 +142,10 @@ const Upload: React.FC<UploadProps> = (props) => {
                 style={{ display: 'none' }}
             >
             </input>
+            <UploadList
+                fileList={fileList}
+                onRemove={handleRemove}
+            />
         </div>
     )
 }
---
+++ src/components/upload/UploadList.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { UploadFile } from './Upload'
+import Icon from '../icon/Icon'
+import Progress from '../progress/Progress'
+
+interface UploadListProps {
+    fileList: UploadFile[]
+    onRemove: (_file: UploadFile) => void
+}
+
+const UploadList: React.FC<UploadListProps> = (props) => {
+    const { fileList, onRemove } = props
+    return (
+        <ul className='web-upload-list'>
+            {fileList.map(item => (
+                <li className='web-upload-list-item' key={item.uid}>
+                    <span className={`file-name file-name-${item.status}`}>
+                        <Icon icon='file-alt' theme='secondary' />
+                        {item.name}
+                    </span>
+                    <span className='file-status'>
+                        {(item.status === 'uploading' || item.status === 'ready') && <Icon icon="spinner" spin theme="primary" />}
+                        {item.status === 'success' && <Icon icon="check-circle" theme="success" />}
+                        {item.status === 'error' && <Icon icon="times-circle" theme="danger" />}
+                    </span>
+                    <span className='file-actions'>
+                        <Icon icon="times" onClick={() => { onRemove(item) }} />
+                    </span>
+                    {item.status === 'uploading' &&
+                        <Progress
+                            percent={item.percent || 0}
+                        />
+                    }
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default UploadList
